b4dd2a1b078e09623b18bac14d1dd355deff2bd6
Lucas <lucasdpau@gmail.com>
2020-03-15 17:38:58 -0400

    chagne to admin.py for extend user profile

--- app1/admin.py
+++ app1/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
-from .models import Tweet
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import Tweet, Profile
 # Register your models here with the built in admin site.  
 # use the command python manage.py createsuperuser
 # login at website/admin to be able to adjust all the databases
 
+# Define an inline admin descriptor for Employee model
+# which acts a bit like a singleton
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = "profile"
+    
+#Define a new user admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (EmployeeInline,)
+
+
+#re-register useradmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Tweet)
