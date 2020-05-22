c14aee3a82123528ca836ee6cb0ff7c66338e669
lucas <lucasdpau@gmail.com>
2020-05-22 16:56:19 -0400

    working on js file

--- app1/static/app1/main.js
+++ app1/static/app1/main.js
@@ -2,9 +2,32 @@ console.log("Hi!")
 
 var ajaxFunction = function() {
 	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+	if (this.readyState == 4 && this.status == 200) {
+		ajaxTestButton.innerHTML = this.responseText;
+		}
+	};
+	xhttp.open("GET", "/follow/q", true);
+	xhttp.send();
 };
 
 ajaxTestButton = document.getElementById("jstest");
-ajaxTestButton.onclick = function() {
-	console.log('asd');
-};
+ajaxTestButton.onclick = ajaxFunction;
+
+
+
+
+var likeTweet = function() {
+
+}
+
+var likeButtons = document.querySelectorAll(".like_button");
+for (var i=0; i<likeButtons.length; i++) {
+	likeButtons[i].onclick = function(){}
+}
+
+
+var unlikeButtons = document.querySelectorAll(".unlike_button");
+for (var i=0; i<unlikeButtons.length; i++) {
+	unlikeButtons[i].onclick = function(){}
+}
