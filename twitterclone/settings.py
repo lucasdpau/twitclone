b66ef270afb858c8d1437c376b5763901818997e
lucas <lucasdpau@gmail.com>
2020-06-01 13:33:38 -0400

    add django-heroku settings to settings.py

--- twitterclone/settings.py
+++ twitterclone/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
-import os
+import os, django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -132,3 +132,5 @@ STATIC_URL = '/static/'
 #)
 
 LOGIN_URL = '/login'
+
+django_heroku.settings(locals())
