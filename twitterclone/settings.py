e422389e57d47457d7d02b87929fe1ceaa58a9d9
lucas <lucasdpau@gmail.com>
2020-06-01 13:13:55 -0400

    runtime = pytho3.7.7 , comment out static_files_dir

--- twitterclone/settings.py
+++ twitterclone/settings.py
@@ -127,8 +127,8 @@ STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+#STATICFILES_DIRS = (
+#   os.path.join(BASE_DIR, 'static'),
+#)
 
 LOGIN_URL = '/login'
